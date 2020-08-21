881ef25ca92406fe1274301e2e28cc2c32b6fd05
KKekitinisa <kkekitinisa@gmail.com>
2020-08-21 13:45:46 +0000

    database has been created

---
+++ service1/application/models.py
@@ -0,0 +1,10 @@
+from application import db
+
+class Fortunes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color=db.Column(db.String(30))
+    starsign=db.Column(db.String(30))
+    fortune=db.Column(db.String(50))
+    
+    def __repr__(self):
+        return ' '.join('Color:',self.color,'Starsign:',self.starsign)
