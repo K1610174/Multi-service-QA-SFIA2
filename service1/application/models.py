2c582536a45fad1863e4250234de03bc831e4a8a
KKekitinisa <kkekitinisa@gmail.com>
2020-08-26 10:49:58 +0000

    Updated the database table

--- service1/application/models.py
+++ service1/application/models.py
@@ -7,4 +7,4 @@ class Fortunes(db.Model):
     fortune=db.Column(db.String(50))
     
     def __repr__(self):
-        return ' '.join('Color:',self.color,'Starsign:',self.starsign)
+        return ' '.join('Color:',self.color,'Starsign:',self.starsign,'Fortune:',self.fortune)
