49a67928c7716c6008879828ecc43d7d037262b8
KKekitinisa <kkekitinisa@gmail.com>
2020-08-26 14:24:27 +0000

    Started unit testing

---
+++ service3/testing/test_mock.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_color(self):
+        with patch('requests.get') as c:
+            c.return_value.text = "Leo"
+
+            response = self.client.get(url_for('starsign'))
+            #self.assertIn(b'red', response.data)
+            self.assertEqual(response.status_code, 200)
