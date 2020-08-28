d21a14b3b1c0b2842ecda4c11472746f0d33508b
AK <kkekitinisa@gmail.com>
2020-08-28 15:54:56 +0000

    trying to deploy using jenkins

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -3,11 +3,11 @@
 #install ansible and run playbook
 sudo apt-get update
 sudo apt-get install python -y
-#mkdir -p ~/.local/bin
-#echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 #sudo chown -R $(whoami):$(whoami) ~/*
-#source ~/.bashrc
-#pip3 install --user ansible
+source ~/.bashrc
+pip3 install --user ansible
 #~/.local/bin/ansible --version
 /var/lib/jenkins/workspace/sfia2/.local/bin/ansible --version
 /var/lib/jenkins/workspace/sfia2/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
