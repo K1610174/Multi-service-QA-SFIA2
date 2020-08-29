f3fa2fecf969a54437ebd6d2f843274f83d2addc
Amanda Kekitinisa <66065549+K1610174@users.noreply.github.com>
2020-08-29 15:36:31 +0100

    Update build_app.sh

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 
 #install ansible and run playbook
-sudo apt-get update
-sudo apt-get install python3 python3-pip -y
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+#sudo apt-get update
+#sudo apt-get install python3 python3-pip -y
+#mkdir -p ~/.local/bin
+#echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 #sudo chown -R $(whoami):$(whoami) ~/*
-source ~/.bashrc
-pip3 install --user ansible
-~/.local/bin/ansible --version
+#source ~/.bashrc
+#pip3 install --user ansible
+#~/.local/bin/ansible --version
 #/var/lib/jenkins/.local/bin/ansible --version
 #/var/lib/jenkins/.local/bin/ansible-playbook -v -i /var/lib/jenkins/workspace/sfia2/inventory.cfg playbook.yaml
+/home/jenkins/.local/bin/ansible --version
+/home/jenkins/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
 docker --version
 docker-compose --version
 
-~/.local/bin/ansible-playbook -i /var/lib/jenkins/workspace/sfia2/inventory.cfg playbook.yaml
+#~/.local/bin/ansible-playbook -i /var/lib/jenkins/workspace/sfia2/inventory.cfg playbook.yaml
 
 #build images using docker
-docker-compose build
+#docker-compose build
 #docker-compose up -d
 #docker login
