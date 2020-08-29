ea544f173ca65b839bf5bc69e377654b89942405
Amanda Kekitinisa <66065549+K1610174@users.noreply.github.com>
2020-08-29 17:43:23 +0100

    Update build_app.sh

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,23 +1,15 @@
 #! /bin/bash
 
 #install ansible and run playbook
-#sudo apt-get update
-#sudo apt-get install python3 python3-pip -y
-#mkdir -p ~/.local/bin
-#echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-#sudo chown -R $(whoami):$(whoami) ~/*
-#source ~/.bashrc
-#pip3 install --user ansible
+sudo apt-get update
+sudo apt-get install python3 python3-pip -y
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+sudo chown -R $(whoami):$(whoami) ~/*
+source ~/.bashrc
+pip3 install --user ansible
 ~/.local/bin/ansible --version
 ~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
-docker --version
-docker-compose --version
-#build images using docker
 export MYSQL_DB=mysql+pymysql://root:1234@database:3306/fortune
 export MYSQL_ROOT_PASSWORD=1234
 export TEST_DB="mysql+pymysql://root:sqldatabase@34.89.57.102:3306/fortune"
-#sudo usermod -aG docker $(whoami)
-#newgrp docker
-#docker-compose build
-#docker-compose up -d
-#docker login
