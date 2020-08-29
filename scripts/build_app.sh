1375857690564aa674d4f859e7ba63728da55c6b
AK <kkekitinisa@gmail.com>
2020-08-29 14:10:15 +0000

    trying to make ansible work

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -8,15 +8,13 @@ echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 #sudo chown -R $(whoami):$(whoami) ~/*
 source ~/.bashrc
 pip3 install --user ansible
-#~/.local/bin/ansible --version
-pwd
-which ansible
-/var/lib/jenkins/.local/bin/ansible --version
-/var/lib/jenkins/.local/bin/ansible-playbook -v -i inventory.cfg playbook.yaml
+~/.local/bin/ansible --version
+#/var/lib/jenkins/.local/bin/ansible --version
+#/var/lib/jenkins/.local/bin/ansible-playbook -v -i /var/lib/jenkins/workspace/sfia2/inventory.cfg playbook.yaml
 docker --version
 docker-compose --version
 
-#~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
+~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
 
 #build images using docker
 docker-compose build
