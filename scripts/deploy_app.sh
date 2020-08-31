86209659d697dc8271489038c07b24fac9f526cf
K1610174 <kkekitinisa@gmail.com>
2020-08-31 22:55:23 +0100

    testing webhook

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -14,11 +14,12 @@ export TEST_DB="$TEST_DB"
 git clone https://github.com/K1610174/QA-SFIA2.git
 cd QA-SFIA2
 
-# docker pull kkeki/service1:latest
-# docker pull kkeki/service2:latest
-# docker pull kkeki/service3:latest
-# docker pull kkeki/service4:latest
-docker-compose ps
+docker pull kkeki/service1:latest
+docker pull kkeki/service2:latest
+docker pull kkeki/service3:latest
+docker pull kkeki/service4:latest
+docker pull kkeki/database:latest
+
 docker stack deploy --compose-file docker-compose.yaml appstack
 docker service scale appstack_service1=2
 docker service scale appstack_service2=2
