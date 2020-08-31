cc17ac0e073d9030b65067f6004d641ec50f655a
K1610174 <kkekitinisa@gmail.com>
2020-08-31 12:47:17 +0100

    database error

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 
 ssh worker-vm-1 << EOF
-git clone https://github.com/K1610174/QA-SFIA2.git
+if [ ! -d QA-SFIA2 ]; then
+    git clone https://github.com/K1610174/QA-SFIA2.git
+fi
 EOF
 ssh worker-vm-2 << EOF
-git clone https://github.com/K1610174/QA-SFIA2.git
+if [ ! -d QA-SFIA2 ]; then
+    git clone https://github.com/K1610174/QA-SFIA2.git
+fi
 EOF
 
 ssh manager-vm-1 << EOF
 export MYSQL_DB="$MYSQL_DB"
 export MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD"
-git clone https://github.com/K1610174/QA-SFIA2.git
+if [ ! -d QA-SFIA2 ]; then
+    git clone https://github.com/K1610174/QA-SFIA2.git
+fi
 cd QA-SFIA2
 docker-compose up -d
 docker-compose ps
-docker exec -it qa-sfia2_service1_1 bash
-python3 create.py
-exit
 docker stack deploy --compose-file docker-compose.yaml appstack
 docker stack services appstack
 cd ..
