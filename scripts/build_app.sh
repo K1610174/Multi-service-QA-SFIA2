d2421b51fae3da69c25734340fc852a81fcbe68b
K1610174 <kkekitinisa@gmail.com>
2020-08-31 16:33:17 +0100

    deploy

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 #install ansible and run playbook
-sudo apt-get update
-sudo apt-get install python3 python3-pip -y
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-sudo chown -R $(whoami):$(whoami) ~/*
-source ~/.bashrc
-pip3 install --user ansible
+#sudo apt-get update
+#sudo apt-get install python3 python3-pip -y
+#mkdir -p ~/.local/bin
+#echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+#sudo chown -R $(whoami):$(whoami) ~/*
+#source ~/.bashrc
+#pip3 install --user ansible
 ~/.local/bin/ansible --version
 ~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
 
@@ -15,5 +15,5 @@ echo "Now building ... "
 docker system prune -f
 docker-compose up -d --build
 docker-compose ps
-docker exec -it qa-sfia2_service1_1 python3 create.py
+docker exec -it sfia2project_service1_1 python3 create.py
 docker images
