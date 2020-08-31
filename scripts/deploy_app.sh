bf7b26d6fba2a2a69380abdacce02dac1e74e62c
K1610174 <kkekitinisa@gmail.com>
2020-08-31 11:51:39 +0100

    credentials in jenkins

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -8,9 +8,7 @@ git clone https://github.com/K1610174/QA-SFIA2.git
 EOF
 
 ssh manager-vm-1 << EOF
-if ![-d QA-SFIA2]; then
-    git clone https://github.com/K1610174/QA-SFIA2.git
-fi
+git clone https://github.com/K1610174/QA-SFIA2.git
 cd QA-SFIA2
 docker stack deploy --compose-file docker-compose.yaml appstack
 docker stack services appstack
