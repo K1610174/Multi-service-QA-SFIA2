7aa30698a8063d7c087005cfb19a37de83ec5a54
Amanda Kekitinisa <66065549+K1610174@users.noreply.github.com>
2020-09-01 08:48:02 +0100

    Update build_app.sh

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 #install ansible and run playbook
-#sudo apt-get update
-#sudo apt-get install python3 python3-pip -y
-#mkdir -p ~/.local/bin
-#echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-#sudo chown -R $(whoami):$(whoami) ~/*
-#source ~/.bashrc
-#pip3 install --user ansible
+sudo apt-get update
+sudo apt-get install python3 python3-pip -y
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+sudo chown -R $(whoami):$(whoami) ~/*
+source ~/.bashrc
+pip3 install --user ansible
 ~/.local/bin/ansible --version
 ~/.local/bin/ansible-playbook -i inventory.cfg playbook.yaml
 
